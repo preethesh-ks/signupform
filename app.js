ea0e90970eaaf9161590c3599a7b76d75027839b
Preeth <crystalclearray@gmail.com>
2021-12-19 20:57:14 +0530

    added email sending feature

--- app.js
+++ app.js
@@ -2,26 +2,54 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const request = require("request");
 const https = require("https");
+const nodemailer = require('nodemailer');
 
 
 const app = express();
-
-
 
 app.use(express.static("public")); ///required to get css and other sources into server //create a folder called public and store css and images there and specify the path leaving public
 app.use(bodyparser.urlencoded({
     extended: true
 })); //initilise body parser to actually work or to tell app yo use bodyparsr
 
+//const mail1 = req.body.email;
+
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/Signup.html")
 });
 
+
+
 app.post("/", function (req, res) {
     const firstName = req.body.first;
     const lastName = req.body.second;
     const mail = req.body.email;
+let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth:{
+        user:"crystalclearray@gmail.com",
+        pass:"crystal9482"
+    },
+    tls:{
+        rejectUnauthorized: false,
+    },
+})
+let mailOptions = {
+    from:"crystalclearray@gmail.com",
+    to: mail,
+    subject:"SIGNED UP TO NEWS LETTER",
+    text:"THANK YOU FOR JOINING OUR NEWSLETTER"
+}
+transporter.sendMail(mailOptions,function(err,sucess){
+    if(err){
+        console.log(error);
+    }
+    else{
+        console.log("email sent sucessfully");
+    }
+})
+
 
 
     const data = {
